f3e2c1d7c3eca793395cf7a3c140404c342cc808
Shravani A <shravania.647@gmail.com>
2025-06-26 12:38:07 +0530

    Initial commit

---
+++ scripts/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDA7BinaP73SfocI7bnt4j_GwIr1x4LNjo",
+  authDomain: "iot-cloud-88c92.firebaseapp.com",
+  databaseURL: "https://iot-cloud-88c92-default-rtdb.firebaseio.com",
+  projectId: "iot-cloud-88c92",
+  storageBucket: "iot-cloud-88c92.appspot.com",
+  messagingSenderId: "201006607302",
+  appId: "1:201006607302:web:a510baa7e9c5e4a9a3e0c7",
+  measurementId: "G-08YBYZ123R"
+};
